1a677d9657e3135d3077e24d8b7768d2e09ee015
AliEenN <ali.nasser.9.1997@gmail.com>
2022-01-23 14:20:30 +0200

    First commit

---
+++ Controllers/CartsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProductCartShop.Models;
+using ProductCartShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCartShop.Controllers
+{
+    public class CartsController : Controller
+    {
+        public CartsController()
+        {
+        }
+
+        // GET
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var cartDetailsVM = new CartDetailsViewModel()
+            {
+                OrderTotal = 0,
+                Count = HttpContext.Session.GetInt32("count") == null ? 0 : (int)HttpContext.Session.GetInt32("count")
+            };
+
+            var str = HttpContext.Session.GetString("product");
+
+            if (str == null)
+                return View(cartDetailsVM);
+
+            var obj = JsonConvert.DeserializeObject<Product>(str);
+
+            cartDetailsVM.Product = obj;
+
+            if (cartDetailsVM.Product.Description.Length > 75)
+            {
+                cartDetailsVM.Product.Description = cartDetailsVM.Product.Description.Substring(0, 74);
+            }
+
+            cartDetailsVM.OrderTotal = cartDetailsVM.Product.Price * cartDetailsVM.Count;
+            cartDetailsVM.TotalAndShippingRate = cartDetailsVM.Product.ShippingRate * cartDetailsVM.Count + cartDetailsVM.OrderTotal;
+
+            return View(cartDetailsVM);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Plus()
+        {
+            var str = HttpContext.Session.GetString("product");
+            var obj = JsonConvert.DeserializeObject<Product>(str);
+
+            var cartDetailsVM = new CartDetailsViewModel()
+            {
+                Count = (int)HttpContext.Session.GetInt32("count") + 1,
+                Product = obj
+            };
+
+            cartDetailsVM.OrderTotal = obj.Price * cartDetailsVM.Count;
+            cartDetailsVM.TotalAndShippingRate = cartDetailsVM.Product.ShippingRate * cartDetailsVM.Count + cartDetailsVM.OrderTotal;
+
+            HttpContext.Session.SetInt32("count", cartDetailsVM.Count);
+
+            return View("Index", cartDetailsVM);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Minus()
+        {
+            var str = HttpContext.Session.GetString("product");
+            var obj = JsonConvert.DeserializeObject<Product>(str);
+
+            var cartDetailsVM = new CartDetailsViewModel()
+            {
+                Count = (int)HttpContext.Session.GetInt32("count") - 1 == 0 ? (int)HttpContext.Session.GetInt32("count") : (int)HttpContext.Session.GetInt32("count") - 1,
+                Product = obj
+            };
+
+            cartDetailsVM.OrderTotal = obj.Price * cartDetailsVM.Count;
+            cartDetailsVM.TotalAndShippingRate = cartDetailsVM.Product.ShippingRate * cartDetailsVM.Count + cartDetailsVM.OrderTotal;
+
+            HttpContext.Session.SetInt32("count", cartDetailsVM.Count);
+
+            return View("Index", cartDetailsVM);
+        }
+        
+        // GET
+        [HttpGet]
+        public IActionResult Continue()
+        {
+            var str = HttpContext.Session.GetString("product");
+            var obj = JsonConvert.DeserializeObject<Product>(str);
+
+            var cartDetailsVM = new CartDetailsViewModel()
+            {
+                Count = (int)HttpContext.Session.GetInt32("count"),
+                Product = obj
+            };
+
+            cartDetailsVM.OrderTotal = cartDetailsVM.Product.Price * cartDetailsVM.Count;
+            cartDetailsVM.TotalAndShippingRate = cartDetailsVM.Product.ShippingRate * cartDetailsVM.Count + cartDetailsVM.OrderTotal;
+            cartDetailsVM.TotalAfterTax = cartDetailsVM.TotalAndShippingRate * (decimal)0.14 + cartDetailsVM.TotalAndShippingRate;
+
+            return View(cartDetailsVM);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ProductCartShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCartShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Product> products;
+        public HomeController()
+        {
+            products = new List<Product>()
+            {
+                new Product { Id = 1, Name = "Car", Description = "Car Description", Price = 1100, ShippingRate = 75 }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult AddToShoppingCarts(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var productExist = products.FirstOrDefault(e => e.Id == id);
+
+            if (productExist == null)
+                return NotFound();
+            
+            var key = "product";
+            var str = JsonConvert.SerializeObject(productExist);
+            HttpContext.Session.SetString(key, str);
+
+            HttpContext.Session.SetInt32("count", 1);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
